f749da1108c74231db37babaf525a598556d07c6
Mykhailo <mykhaylo1809@gmail.com>
2023-01-29 17:49:36 +0200

    Add HelpButton.razor

---
+++ PerJannersten.UI/WpfPages/HelpWindow.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace PerJannersten.UI.WpfPages;
+
+public partial class HelpWindow : Window
+{
+    public HelpWindow()
+    {
+        InitializeComponent();
+    }
+}
